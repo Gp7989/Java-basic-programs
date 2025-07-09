4f77cbb03bc8e9c47080c18e1f0ae10c7ca4ac3b
Gp7989 <pandeygaurav55745@gmail.com>
2025-07-09 11:37:08 +0530

    basic

---
+++ src/ListDemo.java
@@ -0,0 +1,45 @@
+import java.util.Vector;
+
+/*import java.util.Scanner;
+import java.util.Stack;
+public class ListDemo {
+    public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+       Stack<Integer> list = new Stack<>();
+        list.add(33);
+        list.add(33);
+        list.add(34);
+        list.add(null);
+        list.add(90);
+        for (Integer num : list) {
+            System.out.println(num);
+        }
+        System.out.println( list.push(43));
+        //list.pop();
+        System.out.println(list);
+        System.out.println(list.peek());
+    }
+}
+
+ */
+public class ListDemo {
+    public static void main(String[] args) {
+        Vector<Integer> list =new  Vector<>(11);
+        list.add(33);
+        list.add(33);
+        list.add(34);
+        list.add(null);
+        list.add(90);
+        for ( Integer num : list) {
+            System.out.println(num);
+        }
+        System.out.println(list.capacity());
+        System.out.println(list.get(3));
+        System.out.println(list.set(2,666));
+        System.out.println(list);
+        System.out.println(list.size());
+        System.out.println(list.capacity());
+        System.out.println(list.remove(3));
+        System.out.println(list);
+    }
+}
