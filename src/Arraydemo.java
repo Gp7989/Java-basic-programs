32c32b6054af4847bb6152e8d35280f0d2a85618
Gp7989 <pandeygaurav55745@gmail.com>
2025-09-11 21:19:20 +0530

    basic

--- src/Arraydemo.java
+++ src/Arraydemo.java
@@ -1,12 +1,27 @@
 public class Arraydemo {
     public static void main(String[] args) {
-        int arr[][] = new int[3][4];
-      for(int i=0 ; i<3 ;i++){
-          for (int j=0; j< 4 ; j++){
-              arr[i][j] = (int)(Math.random()*100);
-              System.out.print(arr[i][j]+" ");
-          }
-          System.out.println(" ");
-      }
+  //      int arr[][] = new int[3][4];
+//      for(int i=0 ; i<3 ;i++){
+//          for (int j=0; j< 4 ; j++){
+//              arr[i][j] = (int)(Math.random()*100);
+//              System.out.print(arr[i][j]+" ");
+//          }
+//          System.out.println(" ");
+//      }
+int arr[][] = new int[3][];  // jagged array
+        arr[0] = new int[3];
+        arr[1]= new int[4];
+        arr[2] = new int[2];
+        for(int i=0 ; i< arr.length;i++){
+            for (int j=0; j< arr[i].length ; j++){
+                arr[i][j] = (int)(Math.random()*100);
+            }
+        }
+        for(int n[] : arr){
+            for (int m : n){
+                System.out.print(m + " ");
+            }
+            System.out.println();
+        }
     }
 }
