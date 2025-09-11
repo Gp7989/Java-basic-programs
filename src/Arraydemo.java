77010f4e384d7fb8a61a9ffdee731a6ae479483b
Gp7989 <pandeygaurav55745@gmail.com>
2025-09-11 20:38:43 +0530

    basic

--- src/Arraydemo.java
+++ src/Arraydemo.java
@@ -3,7 +3,7 @@ public class Arraydemo {
         int arr[][] = new int[3][4];
       for(int i=0 ; i<3 ;i++){
           for (int j=0; j< 4 ; j++){
-              arr[i][j] = (int)(Math.random()*10);
+              arr[i][j] = (int)(Math.random()*100);
               System.out.print(arr[i][j]+" ");
           }
           System.out.println(" ");
